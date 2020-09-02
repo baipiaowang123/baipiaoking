f37734c5df8fcbcc4fbf54a81f8646d8ca73a68b
baipiaowang123 <70336816+baipiaowang123@users.noreply.github.com>
2020-09-03 01:45:18 +0800

    Update aria2-bot.sh

--- aria2-bot.sh
+++ aria2-bot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "### docker run aria2-pro ###"
 docker run -d --name aria2-pro  --restart unless-stopped --log-opt max-size=1m --network host -e PUID=$UID -e PGID=$GID  -e RPC_SECRET=1887415157 -e RPC_PORT=6800 -e LISTEN_PORT=6888  -e SPECIAL_MODE=rclone -v ~/aria2-config:/config -v ~/aria2-downloads:/downloads p3terx/aria2-pro
+chmod 777 ~/aria2-config/
 echo "### get tele-conf.json###"
 wget -O ~/.tele-aria2-conf.json https://raw.githubusercontent.com/baipiaowang123/baipiaoking/master/.tele-aria2-conf.json
 echo "### docker run telebot###"
