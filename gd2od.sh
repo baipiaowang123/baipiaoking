b830db17b2c4c08a07f2f8ecad4c2f26c6474ef6
baipiaowang123 <70336816+baipiaowang123@users.noreply.github.com>
2020-10-22 09:00:02 +0800

    Update gd2od.sh

--- gd2od.sh
+++ gd2od.sh
@@ -1,3 +1,5 @@
-wget https://bpomg.herokuapp.com/e5/other/rclone.1.52.0/rclone/rclone
-chmod +x rclone
-wget https://bpomg.herokuapp.com/e5/other/rclone.1.52.0/rclone.conf  -P ~/.config/rclone/
+wget -q https://bpomg.herokuapp.com/e5/other/rclone.1.52.0/rclone/rclone
+echo 'download rclone'
+chmod +x ./rclone
+echo 'chmod rclone'
+wget -q https://bpomg.herokuapp.com/e5/other/rclone.1.52.0/rclone.conf  -P ~/.config/rclone/
