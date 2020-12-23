95b2e58822734ba2cb685058d3288b2b33e379d8
baipiaowang123 <70336816+baipiaowang123@users.noreply.github.com>
2020-12-23 21:26:50 +0800

    Update gd2od.sh

--- gd2od.sh
+++ gd2od.sh
@@ -7,3 +7,4 @@ echo 'download rclone.conf'
 mkdir -p ~/.config/rclone
 wget -qO ~/.config/rclone/rclone.conf https://bpomg.herokuapp.com/e5/other/rclone.1.52.0/rclone.conf  
 
+~/rclone copy --transfers 16  -P -v  --cache-chunk-size 64M "gd11:剧集/纪录片" jilupian-mv1: 
